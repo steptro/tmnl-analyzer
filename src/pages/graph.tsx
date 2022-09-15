65927dba0830890f251518c8727441ab2ad9cd53
jesse de jong <jesse.dejong@tmnl.nl>
2022-09-15 16:05:55 +0200

    update style

--- src/pages/graph.tsx
+++ src/pages/graph.tsx
@@ -21,7 +21,7 @@ function Graph() {
     },
     {
       selector: 'node[type = "account"]',
-      style: {
+      css: {
         "background-image": "/account.png",
       },
     },
@@ -30,6 +30,28 @@ function Graph() {
       css: {
         "background-image": "/customer.png",
       },
+    },
+    {
+      selector: '[annotation = "suspect_aml"]',
+      css: {
+          'background-color': 'red',
+          'line-color': 'red'
+      }
+    },
+    {
+        'selector': '[annotation = "new_transaction"]',
+        style: {
+            'background-color': 'red',
+            'line-color': 'red',
+            'line-style': 'dashed',
+        }
+    },
+    {
+        selector: '[cdd_risk > 3]',
+        style: {
+            'background-color': 'red',
+            'shape': 'triangle'
+        }
     },
   ];
 
@@ -52,27 +74,27 @@ function Graph() {
     { data: { id: "8", type: "account", label: "account_8" } },
     { data: { id: "9", type: "account", label: "account_9" } },
     { data: { id: "10", type: "account", label: "account_10" } },
-    { data: { id: "11", type: "customer", label: "Julie Johnston", cdd_risk: 1, country: "MT" } },
+    { data: { id: "11", type: "customer", label: "Julie Johnston", cdd_risk: 5, country: "MT" } },
     { data: { id: "12", type: "customer", label: "Jennifer Neilson", cdd_risk: 1, country: "NL" } },
-    { data: { id: "13", type: "customer", label: "Roger Paletta", cdd_risk: 5, country: "NL" } },
-    { data: { id: "14", type: "customer", label: "Jimmy Caron", cdd_risk: 1, country: "NL" } },
+    { data: { id: "13", type: "customer", label: "Roger Paletta", cdd_risk: 1, country: "NL" } },
+    { data: { id: "14", type: "customer", label: "Jimmy Caron", cdd_risk: 1, country: "MT" } },
     { data: { id: "15", type: "customer", label: "Bobby Harewood", cdd_risk: 1, country: "BE" } },
     { data: { id: "16", type: "customer", label: "Shannon Portis", cdd_risk: 1, country: "NL" } },
     { data: { id: "17", type: "customer", label: "Donald Stahl", cdd_risk: 1, country: "NL" } },
-    { data: { id: "18", type: "customer", label: "George Gallo", cdd_risk: 5, country: "NL" } },
-    { data: { id: "19", type: "customer", label: "William Smart", cdd_risk: 1, country: "RU" } },
+    { data: { id: "18", type: "customer", label: "George Gallo", cdd_risk: 1, country: "NL" } },
+    { data: { id: "19", type: "customer", label: "William Smart", cdd_risk: 5, country: "RU" } },
     { data: { id: "20", type: "customer", label: "Emma Carovski", cdd_risk: 1, country: "NL" } },
-    { data: { source: "1", target: "3", type: "transaction", annotation: ["suspect_aml"] } },
+    { data: { source: "1", target: "3", type: "transaction", annotation: "suspect_aml" } },
     { data: { source: "2", target: "3", type: "transaction" } },
     { data: { source: "3", target: "4", type: "transaction" } },
     { data: { source: "4", target: "5", type: "transaction" } },
     { data: { source: "5", target: "6", type: "transaction" } },
-    { data: { source: "3", target: "6", type: "transaction", annotation: ["suspect_aml"] } },
-    { data: { source: "7", target: "8", type: "transaction", annotation: ["suspect_aml"] } },
-    { data: { source: "8", target: "9", type: "transaction", annotation: ["suspect_aml"] } },
+    { data: { source: "3", target: "6", type: "transaction", annotation: "suspect_aml" } },
+    { data: { source: "7", target: "8", type: "transaction", annotation: "suspect_aml" } },
+    { data: { source: "8", target: "9", type: "transaction", annotation: "suspect_aml" } },
     { data: { source: "9", target: "10", type: "transaction" } },
     { data: { source: "8", target: "10", type: "transaction" } },
-    { data: { source: "6", target: "7", type: "transaction", annotation: ["suspect_aml"] } },
+    { data: { source: "6", target: "7", type: "transaction", annotation: "new_transaction" } },
     { data: { source: "1", target: "11", type: "is_customer" } },
     { data: { source: "2", target: "12", type: "is_customer" } },
     { data: { source: "3", target: "13", type: "is_customer" } },
