0050e544b87afb8c70624277ac9a504ccf86827e
Michael Uryukin <michael.uryukin@tmnl.nl>
2022-09-16 12:08:11 +0200

    added go back

--- src/pages/graph2.tsx
+++ src/pages/graph2.tsx
@@ -157,10 +157,10 @@ function Graph2() {
       <div className="bg-white rounded-md p-4">
         <h2 className="text-2xl mb-2">Community visualization</h2>
         <button
-          onClick={() => navigate("/graph2")}
+          onClick={() => navigate("/graph")}
           className="rounded-md border border-gray-300 bg-white py-2 px-4 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
         >
-          Fetch related news
+          Go back
         </button>
         {loading && <div>Loading</div>}
         {error && <div>Error</div>}
