139e594eb31488930b8fa0e0a051ff5cc22e69f2
Stephan Tromer <stephan.tromer@tmnl.nl>
2022-09-16 11:16:47 +0200

    Fix syntax

--- src/pages/graph.tsx
+++ src/pages/graph.tsx
@@ -11,9 +11,9 @@ function Graph() {
     {
       selector: "node",
       css: {
-        "label": "data(label)",
-        "height": 80,
-        "width": 80,
+        label: "data(label)",
+        height: 80,
+        width: 80,
         "background-fit": "cover",
         "background-opacity": 0.7,
         "border-color": "#000",
@@ -24,13 +24,13 @@ function Graph() {
       },
     },
     {
-      selector : 'edge',
-        css: {
-          'curve-style': 'bezier',
-          'width': 6,
-          'target-arrow-shape': 'triangle',
-          'line-color': 'black',
-          'target-arrow-color': 'black'
+      selector: "edge",
+      css: {
+        "curve-style": "bezier",
+        width: 6,
+        "target-arrow-shape": "triangle",
+        "line-color": "black",
+        "target-arrow-color": "black",
       },
     },
     {
@@ -50,31 +50,31 @@ function Graph() {
     {
       selector: '[annotation = "suspect_aml"]',
       css: {
-          'background-color': 'red',
-          'line-color': 'red',
-          'target-arrow-color': 'red'
-      }
+        "background-color": "red",
+        "line-color": "red",
+        "target-arrow-color": "red",
+      },
     },
     {
       selector: '[annotation = "new_transaction"]',
       css: {
-          'label': "new transaction",
-          'line-color': 'red',
-          'line-style': 'dashed',
-          'target-arrow-color': 'red'
-      }
+        label: "new transaction",
+        "line-color": "red",
+        "line-style": "dashed",
+        "target-arrow-color": "red",
+      },
     },
     {
-      selector: '[cdd_risk > 3]',
+      selector: "[cdd_risk > 3]",
       css: {
-          'background-color': 'red',
-      }
+        "background-color": "red",
+      },
     },
-      {
+    {
       selector: '[type = "is_customer"]',
       css: {
-        'target-arrow-shape': "none"
-      }
+        "target-arrow-shape": "none",
+      },
     },
   ];
 
@@ -82,35 +82,35 @@ function Graph() {
     name: "cose",
     directed: false,
     padding: 10,
-    animate: 'end',
+    animate: "end",
     randomize: true,
   };
 
   const elements = [
     // communities
-    { data: {id: 'A', label: 'community_a'}},
-    { data: {id: 'B', label: 'community_b'}},
+    { data: { id: "A", label: "community_a" } },
+    { data: { id: "B", label: "community_b" } },
     // nodes
-    { data: { id: "1", type: "account", label: "account_1" , parent: "A"} },
-    { data: { id: "2", type: "account", label: "account_2" , parent: "A"} },
-    { data: { id: "3", type: "account", label: "account_3" , parent: "A"} },
-    { data: { id: "4", type: "account", label: "account_4" , parent: "A"} },
-    { data: { id: "5", type: "account", label: "account_5" , parent: "A"} },
-    { data: { id: "6", type: "account", label: "account_6" , parent: "A"} },
-    { data: { id: "7", type: "account", label: "account_7" , parent: "B"} },
-    { data: { id: "8", type: "account", label: "account_8" , parent: "B"} },
-    { data: { id: "9", type: "account", label: "account_9" , parent: "B"} },
-    { data: { id: "10", type: "account", label: "account_10" , parent: "B"} },
-    { data: { id: "11", type: "customer", label: "Julie Johnston", cdd_risk: 5, country: "MT" , parent: "A"} },
-    { data: { id: "12", type: "customer", label: "Jennifer Neilson", cdd_risk: 1, country: "NL" , parent: "A"} },
-    { data: { id: "13", type: "customer", label: "Roger Paletta", cdd_risk: 1, country: "NL" , parent: "A"} },
-    { data: { id: "14", type: "customer", label: "Jimmy Caron", cdd_risk: 1, country: "MT" , parent: "A"} },
-    { data: { id: "15", type: "customer", label: "Bobby Harewood", cdd_risk: 1, country: "BE" , parent: "A"} },
-    { data: { id: "16", type: "customer", label: "Shannon Portis", cdd_risk: 1, country: "NL" , parent: "A"} },
-    { data: { id: "17", type: "customer", label: "Donald Stahl", cdd_risk: 1, country: "NL" , parent: "B"} },
-    { data: { id: "18", type: "customer", label: "George Gallo", cdd_risk: 1, country: "NL" , parent: "B"} },
-    { data: { id: "19", type: "customer", label: "William Smart", cdd_risk: 5, country: "RU" , parent: "B"} },
-    { data: { id: "20", type: "customer", label: "Emma Carovski", cdd_risk: 1, country: "NL" , parent: "B"} },
+    { data: { id: "1", type: "account", label: "account_1", parent: "A" } },
+    { data: { id: "2", type: "account", label: "account_2", parent: "A" } },
+    { data: { id: "3", type: "account", label: "account_3", parent: "A" } },
+    { data: { id: "4", type: "account", label: "account_4", parent: "A" } },
+    { data: { id: "5", type: "account", label: "account_5", parent: "A" } },
+    { data: { id: "6", type: "account", label: "account_6", parent: "A" } },
+    { data: { id: "7", type: "account", label: "account_7", parent: "B" } },
+    { data: { id: "8", type: "account", label: "account_8", parent: "B" } },
+    { data: { id: "9", type: "account", label: "account_9", parent: "B" } },
+    { data: { id: "10", type: "account", label: "account_10", parent: "B" } },
+    { data: { id: "11", type: "customer", label: "Julie Johnston", cdd_risk: 5, country: "MT", parent: "A" } },
+    { data: { id: "12", type: "customer", label: "Jennifer Neilson", cdd_risk: 1, country: "NL", parent: "A" } },
+    { data: { id: "13", type: "customer", label: "Roger Paletta", cdd_risk: 1, country: "NL", parent: "A" } },
+    { data: { id: "14", type: "customer", label: "Jimmy Caron", cdd_risk: 1, country: "MT", parent: "A" } },
+    { data: { id: "15", type: "customer", label: "Bobby Harewood", cdd_risk: 1, country: "BE", parent: "A" } },
+    { data: { id: "16", type: "customer", label: "Shannon Portis", cdd_risk: 1, country: "NL", parent: "A" } },
+    { data: { id: "17", type: "customer", label: "Donald Stahl", cdd_risk: 1, country: "NL", parent: "B" } },
+    { data: { id: "18", type: "customer", label: "George Gallo", cdd_risk: 1, country: "NL", parent: "B" } },
+    { data: { id: "19", type: "customer", label: "William Smart", cdd_risk: 5, country: "RU", parent: "B" } },
+    { data: { id: "20", type: "customer", label: "Emma Carovski", cdd_risk: 1, country: "NL", parent: "B" } },
     // edges
     { data: { source: "1", target: "3", type: "transaction", annotation: "suspect_aml" } },
     { data: { source: "2", target: "3", type: "transaction" } },
@@ -144,16 +144,18 @@ function Graph() {
         {error && <div>Error</div>}
 
         {!loading && !error && (
-      <div className="bg-white rounded-md">
-        <CytoscapeComponent
-          elements={elements}
-          layout={layout}
-          stylesheet={style}
-          style={{ width: "1080px", height: "1000px" }}
-          className="w-full h-full"
-        />
+          <div className="bg-white rounded-md">
+            <CytoscapeComponent
+              elements={elements}
+              layout={layout}
+              stylesheet={style}
+              style={{ width: "1080px", height: "1000px" }}
+              className="w-full h-full"
+            />
+          </div>
         )}
-      </div>
+      </div>{" "}
+      )
     </Layout>
   );
 }
