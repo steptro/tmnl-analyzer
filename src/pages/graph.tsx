457c2c319eab5901146ba4a096637a7394994ee0
jesse de jong <jesse.dejong@tmnl.nl>
2022-09-16 13:46:03 +0200

    New graph title

--- src/pages/graph.tsx
+++ src/pages/graph.tsx
@@ -171,7 +171,7 @@ function Graph() {
 
         {!loading && !error && (
           <div className="bg-white rounded-md">
-            <h2 className="text-2xl mb-2">Community visualization</h2>
+            <h2 className="text-2xl mb-2">Transaction Network</h2>
             <button
               onClick={() => navigate("/graph2")}
               className="rounded-md border border-gray-300 bg-white py-2 px-4 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
--- src/pages/graph2.tsx
+++ src/pages/graph2.tsx
@@ -155,7 +155,7 @@ function Graph2() {
   return (
     <Layout>
       <div className="bg-white rounded-md p-4">
-        <h2 className="text-2xl mb-2">Community visualization</h2>
+        <h2 className="text-2xl mb-2">Transaction Network</h2>
         <button
           onClick={() => navigate("/graph")}
           className="rounded-md border border-gray-300 bg-white py-2 px-4 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
